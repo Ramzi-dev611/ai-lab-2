91f40237af04e6a3376485e750313590de26d37d
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2021-12-14 21:34:52 +0100

    Adding a plotting of number of visited states for each iteration done by the CPU

--- src/game.py
+++ src/game.py
@@ -1,10 +1,11 @@
+from matplotlib import pyplot as plt
 from minmax import MinMaxPredictor
 
 
 class Game:
     size: int
     state: list
-    actions = {}
+    visited_per_iteration: list = []
 
     def __init__(self, size):
         self.size = size
@@ -45,12 +46,14 @@ class Game:
 
     def cpu_find_best_move(self):
         MinMaxPredictor.visited_states = {}
-        action, evaluation = MinMaxPredictor.minmax(self.state, False)
+        action, evaluation, visited_states = MinMaxPredictor.minmax(self.state, False)
+        self.visited_per_iteration.append(visited_states)
         return action, evaluation
 
     def fast_cpu_find_best_move(self):
         MinMaxPredictor.visited_states = {}
-        action, evaluation = MinMaxPredictor.alpha_beta(self.state, False)
+        action, evaluation, visited_states = MinMaxPredictor.alpha_beta(self.state, False)
+        self.visited_per_iteration.append(visited_states)
         return action, evaluation
 
     def cpu_move(self):
@@ -77,6 +80,7 @@ class Game:
                     print("Good Game, you won")
                 continue
             self.cpu_move()
+        self.plot_result()
 
     def start_fast_game(self):
         game_on = True
@@ -92,3 +96,11 @@ class Game:
                     print("Good Game, you won")
                 continue
             self.fast_cpu_move()
+        self.plot_result()
+
+    def plot_result(self):
+        iterations = []
+        for index, _ in enumerate(self.visited_per_iteration):
+            iterations.append(index)
+        plt.scatter(iterations, self.visited_per_iteration)
+        plt.show()
--- src/minmax.py
+++ src/minmax.py
@@ -40,70 +40,78 @@ class MinMaxPredictor:
                 return 1
 
     @classmethod
-    def minmax(cls, state: list, turn: bool):
+    def minmax(cls, state: list, turn: bool, number_visited_states: int = 0):
         if cls.final(state):
-            return (), cls.utility(state, turn)
+            return (), cls.utility(state, turn), number_visited_states + 1
 
         key = " "
         key = turn and key.join([str(num) for num in state])+" : 1" or key.join([str(num) for num in state])+" : -1"
         if key in cls.visited_states:
-            return (), cls.visited_states[key]
+            return (), cls.visited_states[key], number_visited_states+1
 
         if turn:
             max_evaluation = -1000
             returned_action = ()
+            returned_number = 0
             for action in cls.actions(state):
-                _, evaluation = cls.minmax(cls.result(state, action), not turn)
+                _, evaluation, number = cls.minmax(cls.result(state, action), not turn, number_visited_states)
                 if max_evaluation < evaluation:
                     max_evaluation = evaluation
                     returned_action = action
+                returned_number += number
             cls.visited_states[" ".join([str(num) for num in state])+" : 1"] = max_evaluation
-            return returned_action, max_evaluation
+            return returned_action, max_evaluation, returned_number+1
         else:
             min_evaluation = 10000
             returned_action = ()
+            returned_number = 0
             for action in cls.actions(state):
-                _, evaluation = cls.minmax(cls.result(state, action), not turn)
+                _, evaluation, number = cls.minmax(cls.result(state, action), not turn, number_visited_states)
                 if min_evaluation > evaluation:
                     min_evaluation = evaluation
                     returned_action = action
+                returned_number += number
             cls.visited_states[" ".join([str(num) for num in state]) + " : -1"] = min_evaluation
-            return returned_action, min_evaluation
+            return returned_action, min_evaluation, returned_number+1
 
     @classmethod
-    def alpha_beta(cls, state: list, turn: bool, alpha: int = 10000, beta: int = -10000):
+    def alpha_beta(cls, state: list, turn: bool, alpha: int = 10000, beta: int = -10000, number_visited_states: int = 0):
         if cls.final(state):
-            return (), cls.utility(state, turn)
+            return (), cls.utility(state, turn), number_visited_states + 1
         key = " "
         key = turn and key.join([str(num) for num in state]) + " : 1" or key.join([str(num) for num in state]) + " : -1"
         if key in cls.visited_states:
-            return (), cls.visited_states[key]
+            return (), cls.visited_states[key], number_visited_states + 1
 
         if turn:
             max_evaluation = -1000
             returned_action = ()
+            returned_number = 0
             for action in cls.actions(state):
-                act, evaluation = cls.alpha_beta(cls.result(state, action), not turn, alpha, beta)
+                _, evaluation, number = cls.alpha_beta(cls.result(state, action), not turn, alpha, beta, number_visited_states)
                 if max_evaluation < evaluation:
                     max_evaluation = evaluation
                     returned_action = action
+                returned_number += number
                 alpha = max(alpha, evaluation)
                 if beta <= alpha:
                     break
             cls.visited_states[" ".join([str(num) for num in state]) + " : 1"] = max_evaluation
-            return returned_action, max_evaluation
+            return returned_action, max_evaluation, returned_number + 1
         else:
             min_evaluation = 10000
             returned_action = ()
+            returned_number = 0
             for action in cls.actions(state):
-                taken_action, evaluation = cls.alpha_beta(cls.result(state, action), not turn, alpha, beta)
+                _, evaluation, number = cls.alpha_beta(cls.result(state, action), not turn, alpha, beta, number_visited_states)
                 if min_evaluation > evaluation:
                     min_evaluation = evaluation
                     returned_action = action
+                returned_number += number
                 beta = min(beta, evaluation)
                 if beta <= alpha:
                     break
             cls.visited_states[" ".join([str(num) for num in state]) + " : -1"] = min_evaluation
-            return returned_action, min_evaluation
+            return returned_action, min_evaluation, returned_number + 1
 
 
